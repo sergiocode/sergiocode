251d78b9d296bc5478f2836457e2c6b40ac1a6ed
sergiocode <sergiomr.info@gmail.com>
2022-05-25 20:21:23 +0200

    Mejora del menu del video. Ahora se indica en que sección está

--- src/components/Contenido/SobreMi-Item.vue
+++ src/components/Contenido/SobreMi-Item.vue
@@ -14,15 +14,15 @@
                 <div class="botonera-video">
                     <button class="control-button" @click="playVideo()"><img src="../../assets/img/play.png"></button>
                     <!-- 0:00 -->
-                    <button class="section-button" @click="quienSoy()">¿Quien soy?</button>
+                    <button class="section-button" @click="quienSoy()" id="boton-quienSoy">¿Quien soy?</button>
                     <!-- 0:22 -->
-                    <button class="section-button" @click="skills()">Skills</button>
+                    <button class="section-button" @click="skills()" id="boton-skills">Skills</button>
                     <!-- 0:26 -->
-                    <button class="section-button" @click="lenguajes()">Lenguajes</button>
+                    <button class="section-button" @click="lenguajes()" id="boton-lenguajes">Lenguajes</button>
                     <!-- 0:50 -->
-                    <button class="section-button" @click="objetivos()">Objetivos</button>
+                    <button class="section-button" @click="objetivos()" id="boton-objetivos">Objetivos</button>
                     <!-- 1:00 -->
-                    <button class="section-button" @click="personal()">Personal</button>
+                    <button class="section-button" @click="personal()" id="boton-personal">Personal</button>
                 </div>
             </div>
             <ol>
@@ -62,36 +62,125 @@ export default {
     methods: {
         playVideo() {
             let videoElement = document.getElementById("video");
+            let quienSoy = document.getElementById("boton-quienSoy");
+            let skills = document.getElementById("boton-skills");
+            let lenguajes = document.getElementById("boton-lenguajes");
+            let objetivos = document.getElementById("boton-objetivos");
+            let personal = document.getElementById("boton-personal");
+
             if (videoElement.paused) {
-                videoElement.play()
+                videoElement.play();
             } else {
                 videoElement.pause();
             }
+
+            let i = setInterval(() => {
+                if (videoElement.currentTime <= 20) {
+                    quienSoy.style.backgroundColor = "rgb(59,189,174)";
+                } else {
+                    quienSoy.style.backgroundColor = "rgb(69 76 75)";
+                }
+
+                if (videoElement.currentTime >= 20 && videoElement.currentTime <= 26) {
+                    skills.style.backgroundColor = "rgb(59,189,174)";
+                } else {
+                    skills.style.backgroundColor = "rgb(69 76 75)";
+                }
+
+                if (videoElement.currentTime >= 26 && videoElement.currentTime <= 49) {
+                    lenguajes.style.backgroundColor = "rgb(59,189,174)";
+                } else {
+                    lenguajes.style.backgroundColor = "rgb(69 76 75)";
+                }
+
+                if (videoElement.currentTime >= 49 && videoElement.currentTime <= 59) {
+                    objetivos.style.backgroundColor = "rgb(59,189,174)";
+                } else {
+                    objetivos.style.backgroundColor = "rgb(69 76 75)";
+                }
+
+                if (videoElement.currentTime >= 59 && videoElement.currentTime <= 78) {
+                    personal.style.backgroundColor = "rgb(59,189,174)";
+                } else {
+                    personal.style.backgroundColor = "rgb(69 76 75)";
+                }
+            }, 100);
+            i;
         },
         quienSoy() {
             let videoElement = document.getElementById("video");
+            let boton = document.getElementById("boton-quienSoy");
             videoElement.currentTime = 0;
+            boton.style.backgroundColor = "rgb(59,189,174)";
             videoElement.play();
+            let i = setInterval(() => {
+                if (videoElement.currentTime >= 21) {
+                    videoElement.pause();
+                    boton.style.backgroundColor = "rgb(69 76 75)";
+                    clearInterval(i);
+                }
+            }, 500);
+            i;
         },
         skills() {
             let videoElement = document.getElementById("video");
+            let boton = document.getElementById("boton-skills");
             videoElement.currentTime = 21;
+            boton.style.backgroundColor = "rgb(59,189,174)";
             videoElement.play();
+            let i = setInterval(() => {
+                if (videoElement.currentTime >= 26 || videoElement.currentTime <= 21) {
+                    videoElement.pause();
+                    boton.style.backgroundColor = "rgb(69 76 75)";
+                    clearInterval(i);
+                }
+            }, 500);
+            i;
         },
         lenguajes() {
             let videoElement = document.getElementById("video");
+            let boton = document.getElementById("boton-lenguajes");
             videoElement.currentTime = 26;
+            boton.style.backgroundColor = "rgb(59,189,174)";
             videoElement.play();
+            let i = setInterval(() => {
+                if (videoElement.currentTime >= 49 || videoElement.currentTime <= 26) {
+                    videoElement.pause();
+                    boton.style.backgroundColor = "rgb(69 76 75)";
+                    clearInterval(i);
+                }
+            }, 500);
+            i;
         },
         objetivos() {
             let videoElement = document.getElementById("video");
+            let boton = document.getElementById("boton-objetivos");
             videoElement.currentTime = 50;
+            boton.style.backgroundColor = "rgb(59,189,174)";
             videoElement.play();
+            let i = setInterval(() => {
+                if (videoElement.currentTime >= 60 || videoElement.currentTime <= 50) {
+                    videoElement.pause();
+                    boton.style.backgroundColor = "rgb(69 76 75)";
+                    clearInterval(i);
+                }
+            }, 500);
+            i;
         },
         personal() {
             let videoElement = document.getElementById("video");
-            videoElement.currentTime = 60;
+            let boton = document.getElementById("boton-personal");
+             videoElement.currentTime = 60;
+            boton.style.backgroundColor = "rgb(59,189,174)";
             videoElement.play();
+            let i = setInterval(() => {
+                if (videoElement.currentTime >= 78 || videoElement.currentTime <= 60) {
+                    videoElement.pause();
+                    boton.style.backgroundColor = "rgb(69 76 75)";
+                    clearInterval(i);
+                }
+            }, 500);
+            i;
         }
     },
 }
